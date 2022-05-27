0ed34a6019a56b1bed280dd20b5e9c34da3fe1f4
Donte Ladatto <donte279@gmail.com>
2022-05-27 18:29:31 -0500

    commit tree a444d2a4d7644da9cb2c056b048c24fd212ab476

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves arr
+  arr.each_with_index { |dwarf, index| puts "#{index + 1}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  planeteer_calls.map { |call| call.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls arr
+  if arr.find { |word| word.length > 4 } != nil
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese arr
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.find{ |cheese| arr.include?(cheese) }
 end
